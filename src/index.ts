f939fd7da15ebb259f94000ab79e94df471b395e
Evan Smal <evan.smal@hotmail.com>
2021-06-06 16:22:39 -0400

    Refactored top-level

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as readline from "readline";
 
-import { DataType, Nil, toSymbols } from "./types.js";
+import { DataType, Function, List, Nil, toSymbols } from "./types.js";
 import { createEnvironment, Environment } from "./env.js"
 import { ns } from "./core.js"
 import { readString } from "./reader.js";
@@ -29,77 +29,76 @@ function evaluateAST(input: DataType, env: Environment): DataType {
     }
 }
 
-function evaluate(input: DataType, env: Environment): DataType {
+function evaluateDefinition(ast: List, env: Environment) {
+    const [_, key, value] = ast.values;
+    if (key.kind !== "Symbol") throw new Error("def! key is excepted to be symbol");
+    return env.setValue(key.value, value);
+}
 
-    console.log(env.getTable());
+function evaluateLet(ast: List, env: Environment) {
+    const new_environment = createEnvironment([], [], env);
+    const pairs = ast.values[1];
+    if (pairs.kind !== "List") throw new Error("invalid bind");
 
-    if (input.kind !== "List") {
-        return evaluateAST(input, env);
+    for (let i = 0; i < pairs.values.length; i += 2) {
+        const key = pairs.values[i];
+        const value = pairs.values[i + 1];
+        if (key.kind !== "Symbol") {
+            throw new Error(`unexpected token type: ${key}, expected: symbol`);
+        }
+        new_environment.setValue(key.value, evaluateAST(value, new_environment));
     }
+    return evaluateAST(ast.values[2], new_environment);
+}
 
+function evaluateDo(ast: List, env: Environment) {
+    const values = ast.values.slice(1, -1);
+    return values.map(value => evaluateAST(value, env))[values.length - 1];
+}
+
+function evaluateIf(ast: List, env: Environment) {
+    const condition = evaluateAST(ast.values[1], env);
+    if (condition.kind === "True") return evaluateAST(ast.values[2], env);
+    else if (condition.kind === "False") {
+        return (ast.values.length >= 3 ? evaluateAST(ast.values[3], env) : Nil());
+    }
+    else throw new Error(`if condition evaluated to non-boolean value`);
+}
+
+function evaluateFn(ast: List, env: Environment): Function {
+    const [, args, binds] = ast.values;
+
+    if (args.kind !== "List") throw new Error(`Expected argument list for function def`);
+    const symbols = toSymbols(args);
+
+    return {
+        kind: "Function",
+        apply: (...args: DataType[]) => {
+            const new_environment = createEnvironment(symbols, args, env);
+            return evaluate(binds, new_environment);
+        }
+    };
+}
+
+function evaluate(input: DataType, env: Environment): DataType {
     const ast = input;
-
+    if (ast.kind !== "List") { return evaluateAST(input, env); }
     if (ast.values.length === 0) return input;
-
     if (ast.values.length === 1 && ast.values[0].kind === "Number") return ast.values[0];
-
     else {
         const node = ast.values[0];
         if (node.kind === "Symbol") {
             switch (node.value) {
-                case "def!": {
-                    const [_, key, value] = ast.values;
-                    if (key.kind !== "Symbol") throw new Error("def! key is excepted to be symbol");
-                    return env.setValue(key.value, value);
-                }
-                case "let*": {
-                    const new_environment = createEnvironment([], [], env);
-                    const pairs = ast.values[1];
-                    if (pairs.kind !== "List") throw new Error("invalid bind");
-
-                    for (let i = 0; i < pairs.values.length; i += 2) {
-                        const key = pairs.values[i];
-                        const value = pairs.values[i + 1];
-                        if (key.kind !== "Symbol") {
-                            throw new Error(`unexpected token type: ${key}, expected: symbol`);
-                        }
-                        new_environment.setValue(key.value, evaluateAST(value, new_environment));
-                    }
-                    return evaluateAST(ast.values[2], new_environment);
-                }
-                case "do": {
-                    const values = ast.values.slice(1, -1);
-                    return values.map(value => evaluateAST(value, env))[values.length - 1];
-                }
-                case "if": {
-                    const condition = evaluateAST(ast.values[1], env);
-                    if (condition.kind === "True") return evaluateAST(ast.values[2], env);
-                    else if (condition.kind === "False") {
-                        return (ast.values.length >= 3 ? evaluateAST(ast.values[3], env) : Nil());
-                    }
-                    else throw new Error(`if condition evaluated to non-boolean value`);
-                }
-                case "fn*": {
-                    const [, args, binds] = ast.values;
-
-                    if (args.kind !== "List") throw new Error(`Expected argument list for function def`);
-                    const symbols = toSymbols(args);
-
-                    return {
-                        kind: "Function",
-                        apply: (...args) => {
-                            const new_environment = createEnvironment(symbols, args, env);
-                            return evaluate(binds, new_environment);
-                        }
-                    };
-                }
+                case "def!": { return evaluateDefinition(ast, env); }
+                case "let*": { return evaluateLet(ast, env); }
+                case "do": { return evaluateDo(ast, env); }
+                case "if": { return evaluateIf(ast, env); }
+                case "fn*": { return evaluateFn(ast, env); }
             }
-
         }
     }
 
     const result = evaluateAST(ast, env);
-
     if (result.kind !== "List") throw new Error(`Expected list, got ${result.kind}`);
 
     const [func, ...args] = result.values;
@@ -113,7 +112,6 @@ function print(input: DataType): string {
 
 function rep(input: string, environment: Environment): string {
     const line = print(evaluate(read(input), environment));
-    console.log(line);
     return line;
 }
 
@@ -131,7 +129,6 @@ async function main() {
 
     const default_env = createEnvironment([], []);
     default_env.addSymbols(ns);
-
     while (true) { const input = await getInput(rl); rep(input, default_env); }
 }
 
